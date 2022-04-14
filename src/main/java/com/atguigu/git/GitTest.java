429649f099a82d22d09ae30e3e8f364593feb953
Maxwell <maxwell091116@163.com>
2022-04-14 10:53:47 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
-        System.out.println("git test");
+        System.out.println("git test2");
+        System.out.println("git test3");
 
     }
 }
