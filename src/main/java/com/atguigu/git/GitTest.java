9f4c6cdde91c8d3481cd44c1d133491db8ec9581
Maxwell <maxwell091116@163.com>
2022-04-14 10:52:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zdd
+ * @create 2022-04-14 10:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
