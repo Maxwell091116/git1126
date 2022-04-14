ea3d4b609ec6e92461ef64fdbc5ce5e93b70fbe9
Maxwell <maxwell091116@163.com>
2022-04-14 11:24:24 +0800

    master test3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,8 +10,8 @@ public class GitTest {
         System.out.println("git test2");
         System.out.println("git test3");
         System.out.println("git test4");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test2");
+        System.out.println("master test");
+
 
     }
 }
